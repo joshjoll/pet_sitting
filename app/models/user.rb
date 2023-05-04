006537660816c66c4376fa8d5fd0883938538730
Josh Joll <40342278+joshjoll@users.noreply.github.com>
2023-05-04 14:03:46 -0500

    add user_type to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum user_type: {
+    'customer': 1,
+    'employee': 2
+  }
 end
