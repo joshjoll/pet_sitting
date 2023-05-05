8e5bcf9400d05811a2a83cd3e2266e9a3cf7e563
Josh Joll <40342278+joshjoll@users.noreply.github.com>
2023-05-05 11:19:22 -0500

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,93 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# create users
+users = [
+    {
+        email: "employee@employee.com",
+        first_name: "Perfect",
+        last_name: "Employee",
+        password: "password"
+    },
+    {
+        email: "good_customer@customer.com",
+        first_name: "Pretty Good",
+        last_name: "Customer",
+        password: "password"
+    },
+    {
+        email: "bad_customer@customer.com",
+        first_name: "Less Good",
+        last_name: "Customer",
+        password: "password"
+    }
+]
+
+users.each do |user|
+    new_user = User.find_or_create_by(email: user[:email])
+    new_user.update(user)
+end
+# create animals
+animals = [
+    {
+        animal_type: "Dog",
+        hourly_cost: 10,
+    },
+    {
+        animal_type: "Cat",
+        hourly_cost: 5,
+    }
+]
+
+animals.each do |animal|
+    Animal.find_or_create_by(animal)
+end
+
+customer_one = User.find_by(email: 'good_customer@customer.com').id
+customer_two = User.find_by(email: 'bad_customer@customer.com').id
+
+dog = Animal.find_by(animal_type: 'Dog').id
+cat = Animal.find_by(animal_type: 'Cat').id
+
+sitting_requests = [
+    {
+        user_id: customer_one,
+        animal_id: dog,
+        pet_name: "Quackers",
+        drop_off: DateTime.parse('2023-05-13 09:00:00 AM'),
+        pickup: DateTime.parse('2023-05-13 2:00:00 PM'),
+        request_status: 'accepted',
+        cost: 70
+    },
+    {
+        user_id: customer_one,
+        animal_id: cat,
+        pet_name: "Whiskers",
+        drop_off: DateTime.parse('2023-05-13 09:00:00 AM'),
+        pickup: DateTime.parse('2023-05-13 2:00:00 PM'),
+        request_status: 'accepted',
+        cost: 45
+    },
+    {
+        user_id: customer_two,
+        animal_id: dog,
+        pet_name: "Fence Post",
+        drop_off: DateTime.parse('2023-05-13 11:00:00 AM'),
+        pickup: DateTime.parse('2023-05-13 1:00:01 PM'),
+        request_status: 'cancelled',
+        cost: 40
+    },
+    {
+        user_id: customer_two,
+        animal_id: cat,
+        pet_name: "Stockings",
+        drop_off: DateTime.parse('2023-05-13 11:00:00 PM'),
+        pickup: DateTime.parse('2023-05-14 4:00:00 AM'),
+        request_status: 'rejected',
+        cost: 45
+    }
+]
+sitting_requests.each do |request|
+    SittingRequest.create(request)
+end
