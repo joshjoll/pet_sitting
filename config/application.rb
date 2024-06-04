6804f51abaaf3c9c1a1d5a067d12974fb0ee40ed
Josh Joll <40342278+joshjoll@users.noreply.github.com>
2024-06-04 12:11:46 -0500

    include lib dir in app

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module PetSitting
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths << "#{Rails.root}/lib"
 
     # Configuration for the application, engines, and railties goes here.
     #
